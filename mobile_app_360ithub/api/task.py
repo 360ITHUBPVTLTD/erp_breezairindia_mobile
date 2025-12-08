1616a9c28516ee04491499e9576be9d5f9f2f3ae
mohan madhan <mohan@360ithub.com>
2025-12-08 11:18:57 +0530

    mobile app

--- mobile_app_360ithub/api/task.py
+++ mobile_app_360ithub/api/task.py
@@ -1,14 +1,14 @@
 import frappe
 from .common import get_doc_with_filters
 from frappe.utils import getdate, today
-
+ 
 @frappe.whitelist()
 def get_task_summary():
     # user = frappe.session.user
     # tesks = get_doc_with_filters("Task", filters={"custom_task_owner": user}, limit=999999)
     pending_tasks = get_doc_with_filters("Task", filters=
-[["status","not in",["Completed","Cancelled","Template"]]],  limit=9999999)
-
+[["status","not in",["Template", "Completed", "Cancelled"]]],  limit=9999999)
+ 
     task_without_due_date = [task for task in pending_tasks if not task.get('exp_end_date')]
     overdue_tasks = [
         task
@@ -18,70 +18,42 @@ def get_task_summary():
     ]
     todays_tasks = [task for task in pending_tasks if task.get("exp_end_date") == getdate(today())]
     upcoming_seven_days_tasks = [task for task in pending_tasks if task.get("exp_end_date") and getdate(task.get("exp_end_date")) > getdate(today()) and getdate(task.get("exp_end_date")) <= getdate(frappe.utils.add_days(today(), 7))]
-
-    last_14_days_tasks = get_doc_with_filters(
-        "Task",
-        filters=[
-            ["status", "not in", ["Cancelled", "Template"]],
-            ["modified", ">=", frappe.utils.add_days(today(), -14)],
-        ],
-        limit=9999999,
-    )
-
-    last_14_days_todo = [
+ 
+    total_todo = [
         task
-        for task in last_14_days_tasks
+        for task in pending_tasks
         if task.get("status") == "Open"
     ]
-    last_14_days_in_progress = [
+    total_in_progress = [
         task
-        for task in last_14_days_tasks
+        for task in pending_tasks
         if task.get("status") == "Working"
     ]
-    last_14_days_review = [
+    total_review = [
         task
-        for task in last_14_days_tasks
+        for task in pending_tasks
         if task.get("status") == "Pending Review"
     ]
-    last_14_days_completed = [
-        task
-        for task in last_14_days_tasks
-        if task.get("status") == "Completed"
-    ]
-    last_14_days_urgent_completed = [
-        task
-        for task in last_14_days_tasks
-        if task.get("status") == "Completed" and task.get("priority") == "Urgent"
-    ]
-    last_14_days_high_completed = [
-        task
-        for task in last_14_days_tasks
-        if task.get("status") == "Completed" and task.get("priority") == "High"
-    ]
-    last_14_days_medium_completed = [
-        task
-        for task in last_14_days_tasks
-        if task.get("status") == "Completed" and task.get("priority") == "Medium"
-    ]
-    last_14_days_low_completed = [
-        task
-        for task in last_14_days_tasks
-        if task.get("status") == "Completed" and task.get("priority") == "Low"
-    ]
-
+    total_completed = get_doc_with_filters("Task", filters=[["status", "=", "Completed"]], limit=9999999)
+ 
+    total_urgent = [task for task in pending_tasks if task.get("priority") == "Urgent"]
+    total_high = [task for task in pending_tasks if task.get("priority") == "High"]
+    total_medium = [task for task in pending_tasks if task.get("priority") == "Medium"]
+    total_low = [task for task in pending_tasks if task.get("priority") == "Low"]
+ 
     return {
-        "total_tasks": len(pending_tasks),
-        "tasks_without_due_date": len(task_without_due_date),
-        "overdue_tasks": len(overdue_tasks),
-        "todays_tasks": len(todays_tasks),
-        "upcoming_seven_days_tasks": len(upcoming_seven_days_tasks),
-        "last_14_days_todo": len(last_14_days_todo),
-        "last_14_days_in_progress": len(last_14_days_in_progress),
-        "last_14_days_review": len(last_14_days_review),
-        "last_14_days_completed": len(last_14_days_completed),
-        "last_14_days_urgent_completed": len(last_14_days_urgent_completed),
-        "last_14_days_high_completed": len(last_14_days_high_completed),
-        "last_14_days_medium_completed": len(last_14_days_medium_completed),
-        "last_14_days_low_completed": len(last_14_days_low_completed),
-        # "last_14_days": last_14_days_tasks
+        "total_tasks_count": len(pending_tasks),
+        "tasks_without_due_date_count": len(task_without_due_date),
+        "overdue_tasks_count": len(overdue_tasks),
+        "todays_tasks_count": len(todays_tasks),
+        "upcoming_seven_days_tasks_count": len(upcoming_seven_days_tasks),
+        "total_todo_count": len(total_todo),
+        "total_in_progress_count": len(total_in_progress),
+        "total_review_count": len(total_review),
+        "total_completed_count": len(total_completed),
+        "total_urgent_count": len(total_urgent),
+        "total_high_count": len(total_high),
+        "total_medium_count": len(total_medium),
+        "total_low_count": len(total_low),
     }
+ 
